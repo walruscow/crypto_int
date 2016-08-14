2cb52b4da7dfa397c41bed61af81a0d8b4e5f8ba
WalrusCow <willgmcd@gmail.com>
2016-08-14 21:35:25 +0100

    Add subtraction for big ints

--- src/bigint/arithmetic.rs
+++ src/bigint/arithmetic.rs
@@ -2,6 +2,7 @@ use std::num::Wrapping;
 
 type W = Wrapping<u64>;
 
+// TODO: Add overflow check?
 pub fn add_big_ints(a: &Vec<u64>, b: &Vec<u64>) -> Vec<u64> {
     let mut overflow = false;
     a.iter().zip(b.iter()).map(move |(x, y)| {
@@ -12,6 +13,20 @@ pub fn add_big_ints(a: &Vec<u64>, b: &Vec<u64>) -> Vec<u64> {
         };
         // Digit overflowed iff result is less than either of the operands
         overflow = digit < *x;
+        digit
+    }).collect()
+}
+
+pub fn sub_big_ints(a: &Vec<u64>, b: &Vec<u64>) -> Vec<u64> {
+    let mut underflow = false;
+    a.iter().zip(b.iter()).map(move |(x, y)| {
+        let digit = if underflow {
+            x.wrapping_sub(*y).wrapping_sub(1)
+        } else {
+            x.wrapping_sub(*y)
+        };
+        // Digit underflowed iff result is more than the original value
+        underflow = digit > *x;
         digit
     }).collect()
 }
